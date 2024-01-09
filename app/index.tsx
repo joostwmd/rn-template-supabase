e7da0d65ff464ed62ec30e5055ba97fa0c66c89c
joostwmd <88150245+joostwmd@users.noreply.github.com>
2024-01-09 18:30:16 +0100

    chore: cleanup

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "../components/Themed";
+
+const Page: React.FC = () => {
+  return (
+    <View style={{ flex: 1 }}>
+      <Text>Welcome</Text>
+    </View>
+  );
+};
+
+export default Page;
