4018cb9eb14eebe66754815b0a67cc702de3ddc6
joostwmd <88150245+joostwmd@users.noreply.github.com>
2024-01-09 19:51:10 +0100

    feat: logout button

--- app/(protected)/profile.tsx
+++ app/(protected)/profile.tsx
@@ -1,8 +1,38 @@
-import React from "react";
-import { Text } from "react-native";
+import React, { useEffect } from "react";
+import { Button, Text, View } from "react-native";
+import { supabase } from "../../lib/utils/supabase";
+import { useRouter } from "expo-router";
 
 function Profile() {
-  return <Text>Profile page </Text>;
+  async function logOut() {
+    await supabase.auth.signOut();
+  }
+  const router = useRouter();
+  useEffect(() => {
+    const getSession = async () => {
+      try {
+        const { data, error } = await supabase.auth.getSession();
+        console.log(data.session);
+        if (error) {
+          throw error;
+        } else if (data.session) {
+          router.push("/(protected)/profile");
+        }
+      } catch (error) {
+        // Handle error
+      }
+    };
+
+    getSession();
+  }, []);
+
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Profile page</Text>
+
+      <Button title="Log out" onPress={logOut} />
+    </View>
+  );
 }
 
 export default Profile;
