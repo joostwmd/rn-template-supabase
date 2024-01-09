d647ce40994f9d8cf09f58957aca0620edab6ba6
joostwmd <88150245+joostwmd@users.noreply.github.com>
2024-01-09 19:13:17 +0100

    chore: public and protected dirs

---
+++ app/(public)/index.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from "react-native";
+import { Auth } from "../../lib/components/Auth";
+import { useEffect } from "react";
+import { supabase } from "../../lib/utils/supabase";
+
+const Page: React.FC = () => {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Auth />
+    </View>
+  );
+};
+
+export default Page;
