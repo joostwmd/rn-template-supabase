541eb35a32eec9cad79f48c169116e0a85eb935f
joostwmd <88150245+joostwmd@users.noreply.github.com>
2024-01-09 18:54:42 +0100

    feat: apple native auth

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,12 @@
 import { View, Text } from "react-native";
+import { Auth } from "../lib/components/Auth";
+import { useEffect } from "react";
+import { supabase } from "../lib/utils/supabase";
 
 const Page: React.FC = () => {
   return (
-    <View style={{ flex: 1 }}>
-      <Text>Welcome</Text>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Auth />
     </View>
   );
 };
